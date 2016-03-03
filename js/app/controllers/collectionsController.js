7f1a8274ffa7ef062e4aaf071167c18e29c7e27c
Shibut Sergey (home) <shsa@metib.ru>
2016-03-03 23:44:39 +0300

    repair bugs in collections with modal!

--- js/app/controllers/collectionsController.js
+++ js/app/controllers/collectionsController.js
@@ -9,16 +9,42 @@ angular.module('app').controller('collectionsController', [function(){
 
     var collection = kondakova.collections[number_of_collection], divs = '';
 
+
+    // divs =     '<div class="modal fade" id="product-quick-view-modal" tabindex="-1" role="dialog" aria-hidden="false" style=""> \
+    //   <div class="modal-dialog modal-lg"> \
+    //     <div class="modal-content"> \
+    //       <div class="modal-body"> \
+    //         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button> \
+    //         <div class="row quick-view product-main"> \
+    //           <div class="col-sm-12"> \
+    //             <div class="quick-view-main-image"> \
+    //               <img src="img/detailbig1.jpg" alt="" class="img-responsive"> \
+    //             </div> \
+    //           </div> \
+    //         </div> \
+    //       </div> \
+    //     </div> \
+    //   </div> \
+    // </div>';
+
+
     $('#collections-content').empty();
     $('#collections').find('h3').text(collection.name);
 
     $.each(collection.images, function(index, val) {
-      divs += "<div class='col-md-3 col-sm-4'><div class='product'><div class='image'>" +
-              "<a href='#modal' data-toggle='modal' data-target='#product-quick-view-modal'>"+
-              "<img src='" + val + "' alt='photo from collection' class='img-responsive image1'></a>" +
-              "<div class='quick-view-button'>" +
-              "<a href='' data-toggle='modal' data-target='#product-quick-view-modal' " +
-              "class='btn btn-default btn-sm'>Открыть</a></div> </div></div></div>";
+      divs  +=  "<div class='col-md-3 col-sm-4'>" +
+                  "<div class='product'>" +
+                    "<div class='image'>" +
+                      "<a href='' data-toggle='modal' data-target='#product-quick-view-modal' onclick='return false;'>"+
+                         "<img src='" + val + "' alt='photo from collection' class='img-responsive image1'>"+
+                      "</a>" +
+                      "<div class='quick-view-button'>" +
+                        "<a href='' data-toggle='modal' data-target='#product-quick-view-modal' " +
+                        "class='btn btn-default btn-sm' onclick='return false;'>Открыть</a>"+
+                      "</div>"+
+                    "</div>"+
+                  "</div>"+
+                "</div>";
     });
 
 // <div class="col-md-4 col-sm-6">
